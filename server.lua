6a65b2a6e641aea2f206aaad171c93fee0cf2d06
leocroatian <104053610+leocroatian@users.noreply.github.com>
2024-08-17 11:15:20 -0400

    Update server.lua

--- server.lua
+++ server.lua
@@ -7,13 +7,13 @@ AddEventHandler('txAdmin:events:scheduledRestart', function(eventData)
     if eventData.secondsRemaining == 180 and purgeTriggered == false then
         TriggerClientEvent('purge:start', -1)
         purgeTriggered = true
-        print('[DEBUG] Purge has started.')
+        -- print('[DEBUG] Purge has started.')
     end
 end)
 AddEventHandler('txAdmin:events:skippedNextScheduledRestart', function(eventData)
     if purgeTriggered then
         TriggerClientEvent('purge:stop', -1)
         purgeTriggered = false
-        print('[DEBUG] Purge has stopped.')
+        -- print('[DEBUG] Purge has stopped.')
     end
 end)
